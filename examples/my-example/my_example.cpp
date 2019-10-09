211545b2be21d5eec3ced35938e4fcd3af7312f2
Jensen Miller <millerjamtime@hotmail.com>
2019-10-09 02:12:06 -0400

    Basic template

---
+++ examples/my-example/my_example.cpp
@@ -0,0 +1,14 @@
+﻿// TemplatedCMake.cpp : Defines the entry point for the application.
+//
+
+#include "my_example.hpp"
+#include <hello_world.hpp>
+
+
+using namespace std;
+
+int main()
+{
+	say_hello();
+	return 0;
+}
---
+++ src/hello_world.cpp
@@ -0,0 +1,8 @@
+
+#include <iostream>
+#include "hello_world.hpp"
+
+void say_hello()
+{
+	std::cout << "Hello World!" << std::endl;
+}
